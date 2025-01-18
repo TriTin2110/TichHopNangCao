dacaa059bcf80e3dc75c55d3324b636f20a26aae
TriTin <tinnguyen5071@gmail.com>
2025-01-18 08:25:04 +0700

    first commit

---
+++ View-source/js/shopping.js
@@ -0,0 +1,19 @@
+@import url('https://fonts.googleapis.com/ css?family=Roboto');
+body{
+font-family: 'Roboto', sans-serif;
+}
+*{
+margin: 0; padding: 0;
+}
+i {
+margin-right: 10px;
+}
+input:focus, button: focus,
+.form-control:focus{ outline: none; box-shadow: none; }
+.form-control:disabled, .form-control [readonly]{ background-color: #fff;
+}
+.table tr,
+.table tr td{
+vertical-align: middle;
+}
+.button-container{ display: flex;
