0fde96004efcef3ef48c3bfe813da1f59d6c98d5
Thinh Nguyen <ducsu510@gmail.com>
2025-04-26 08:38:13 +0700

    Thêm trang user-view, thêm 3 phương thức GET, PUT, DELETE user

--- View-source/app.js
+++ View-source/app.js
@@ -35,6 +35,7 @@ const upload = multer({ storage: storage })
 insert(upload)
 update(upload)
 app.set('view engine', 'ejs')
+app.set('views', path.join(_dirname, 'views', 'ejs'));
 
 app.use((req, res, next) => {
     res.locals.formatToVND = formatToVND
@@ -49,10 +50,15 @@ app.use(session({
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(path.join(_dirname, '../views'))) //khai báo cho express biết nơi phục vụ file tĩnh có trong thư mục views
 app.use(bodyParser.json())
+app.use(express.json());
 app.use('/user', userRoute);
 app.use('/product', productRoute);
 app.use('/order', orderRoute);
 app.use('/', utilRoute);
+
+app.get('/user', (req, res) => {
+    res.render('user-view');
+});
 
 app.listen(8080, async() => {
     try {
--- View-source/routes/User.js
+++ View-source/routes/User.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { userController } from '../controllers/index.js'
+import UserModel from '../models/User.js'
 
 let userRoute = express()
 
@@ -9,4 +10,50 @@ userRoute.post('/', userController.insert)
 userRoute.post('/update', userController.update)
 userRoute.post('/:name', userController.remove)
 
+userRoute.get('/login-page', (req, res) => {
+    res.render('login', { error: null })
+})
+
+userRoute.get('/view', async (req, res) => {
+    try {
+        const users = await UserModel.find()
+        res.render('./ejs/user-view.ejs', { users, errorInput: null, error: null })
+    } catch (err) {
+        res.status(500).send(`Lỗi khi lấy danh sách người dùng: ${err.message}`)
+    }
+})
+
+userRoute.put('/:id', async (req, res) => {
+    try {
+        const { fullName, phone, email, address, password } = req.body;
+
+        const user = await UserModel.findByIdAndUpdate(
+            req.params.id,
+            { fullName, phone, email, address, password },
+            { new: true } // trả về dữ liệu mới
+        );
+
+        if (!user) {
+            return res.status(404).json({ message: 'Không tìm thấy người dùng' });
+        }
+
+        res.json({ message: 'Cập nhật thành công', user });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error });
+    }
+});
+
+userRoute.delete('/:id', async (req, res) => {
+    try {
+        const user = await UserModel.findByIdAndDelete(req.params.id);
+
+        if (!user) {
+            return res.status(404).json({ message: 'Không tìm thấy người dùng' });
+        }
+
+        res.json({ message: 'Xoá thành công' });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error });
+    }
+});
 export { userRoute }
